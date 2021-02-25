7952893f5f42cb1932deb3ae72bd8b470aeec675
rogithraj107 <rogithraj107@gmail.com>
2021-02-25 23:07:33 +0530

    New Code

--- src/org/git3/GitSample.java
+++ src/org/git3/GitSample.java
@@ -1,10 +1,8 @@
 package org.git3;
 
 public class GitSample {
-	
-	
+
 	public static void main(String[] args) {
-		
 		System.out.println("Test1");
 		System.out.println("Test2");
 		System.out.println("Test4");
